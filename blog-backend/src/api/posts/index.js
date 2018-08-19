253e613050c2aa6fb23d7c522a86709fe2846999
9min <gm0711@nate.com>
2018-08-19 16:29:03 +0900

    mongoose를 이용한 MongoDB 연동 실습

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,9 +5,8 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.delete);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:id', postsCtrl.checkObjectid, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectid, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectid, postsCtrl.update);
 
 module.exports = posts;
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,3 +1,20 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const {
+  PORT: port = 4000,
+  MONGO_URI: mongoURI
+} = process.env;
+
+// Node의 Promise를 사용하도록 설정
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI).then(() => {
+  console.log('connected to mongodb');
+}).catch((e) => {
+  console.error(e);
+});
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
@@ -16,6 +33,6 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('listening to port 4000');
 });
